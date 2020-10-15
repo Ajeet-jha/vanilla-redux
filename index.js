da287403111991635c93c2712b37a309bf3d2f4d
Your Name <you@example.com>
2020-10-15 12:07:26 +0530

    added express

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import { createStore} from 'redux';
-
+import { createStore} from './node_modules/redux';
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
 const initialState = {
     age: 21
 }
@@ -43,4 +46,13 @@ store.dispatch({
 store.dispatch({
     type: "SUB_AGE",
     val: 10
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(app.use(express.static('public')));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
