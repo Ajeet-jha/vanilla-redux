96a4daa8404802c8d96481401c2ee58a1c246db9
Your Name <you@example.com>
2020-10-15 11:37:35 +0530

    redux with vanilla js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log(" redux with vanilla js !!");
+import { createStore} from 'redux';
+
+const initialState = {
+    age: 21
+}
+
+const reducer = (state = initialState, action) => {
+    const newState = {
+        ...state
+    }
+    switch (action.type) {
+        case "ADD_AGE":
+            newState.age += action.val;
+            return {
+                ...newState
+            }
+            break;
+
+        case "SUB_AGE":
+            newState.age -= action.val;
+            return {
+                ...newState
+            }
+            break;
+        default:
+            return {
+                ...state
+            }
+            break;
+    }
+}
+
+const store = createStore(reducer)
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({
+    type: "ADD_AGE",
+    val: 12
+})
+store.dispatch({
+    type: "SUB_AGE",
+    val: 10
+})
